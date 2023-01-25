302778b3a7159001f07bf083e343bb384b6885fc
Brent Watkins <brent.n.watkins@gmail.com>
2023-01-25 12:14:32 -0700

    feature: Created working API calculator functionality with CORS support

--- api/views.py
+++ api/views.py
@@ -1,21 +1,59 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import requests
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import math
 
+# def check_balance()
+#   Get the user's current balance.
+#   This will be used to ensure that the user has enough
+#   credit to perform an operation. HTTP status 402 will
+#   be returned if the user does not have credit available.
+
+@api_view(['POST'])
 def add(request):
-  return HttpResponse('Add')
+  if request.method == 'POST':
+    operands = [int(num) for num in request.data["operands"]]
+    result = sum(operands)
+    return Response(data=result, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
 def divide(request):
-  return HttpResponse('Divide')
+  if request.method == 'POST':
+    operands = [int(num) for num in request.data["operands"]]
+    result = operands[0]
+    for i in range(1, len(operands)):
+      result = result / operands[i]
+    return Response(content_type="application/json", data=result, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
 def multiply(request):
-  return HttpResponse('Multiply')
+  if request.method == 'POST':
+    operands = [int(num) for num in request.data["operands"]]
+    result = operands[0]
+    for i in range(1, len(operands)):
+      result = result * operands[i]
+    return Response(data=result, status=status.HTTP_200_OK)
 
+@api_view(['GET'])
 def random_string(request):
-  return HttpResponse('Random String')
+  if request.method == 'GET':
+    result = requests.get('https://www.random.org/strings/?num=1&len=20&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new')
+    return Response(data=result.text, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
 def square_root(request):
-  return HttpResponse('Square Root')
+  if request.method == 'POST':
+    operand = int(request.data["operands"][0])
+    result = math.sqrt(operand)
+    return Response(data=result, status=status.HTTP_200_OK)
 
+@api_view(['POST'])
 def subtract(request):
-  return HttpResponse('Subtract')
+  if request.method == 'POST':
+    operands = [int(num) for num in request.data["operands"]]
+    result = operands[0]
+    for i in range(1, len(operands)):
+      result = result - operands[i]
+    return Response(data=result, status=status.HTTP_200_OK)
 
--- calculator_back_end/settings.py
+++ calculator_back_end/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
