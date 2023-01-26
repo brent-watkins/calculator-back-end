78ca0f265cea3ff5f7bd1e77772447d000d3e206
Brent Watkins <brent.n.watkins@gmail.com>
2023-01-26 16:44:30 -0700

    feature: Created log in and log out endpoints

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
 path('add/', views.add, name='Add'),
 path('divide/', views.divide, name='Divide'),
+path('login/', views.login, name="Log In"),
+path('logout/', views.logout, name='Log Out'),
 path('multiply/', views.multiply, name='Multiply'),
 path('random_string/', views.random_string, name='Random String'),
 path('register/', views.register, name="Register"),
